987aa6916698c2b59e4a4b14e76a549668d25724
banahidmi <100878926+banahidmi@users.noreply.github.com>
2023-02-23 18:53:10 +0300

    Adding books into the table

--- script.js
+++ script.js
@@ -12,10 +12,7 @@ image.addEventListener('click', function handleClick() {
 });
 //books info
 class Book{
-  Book(){
-
-  }
-  Book(title,author,pages,bookread) {
+  constructor(title,author,pages,bookread) {
     this.title = title;
     this.author= author;
     this.pages= pages;
@@ -23,13 +20,44 @@ class Book{
   }
 
 }
-let PLAYERS = [];
+let BOOKS = [];
+let i=0;
 function addbook(){
   const title=document.getElementById('title').value;
+  console.log(title);
   const author=document.getElementById('author').value;
+  console.log(author);
   const pages=document.getElementById('pages').value;
+  console.log(pages);
   const bookread=document.getElementById('bookread').value;
+  console.log(bookread);
   const b1=new Book(title,author,pages,bookread);
   console.log(b1.author);
+  BOOKS.push(b1);
+  console.log(BOOKS[0]);
+  addtotable(BOOKS);
+}
+
+function addtotable(book1){
+  const tr = document.createElement("tr");
+  const td1= document.createElement("td");
+  const title1 = document.createTextNode(book1[i].title);
+  td1.appendChild(title1);
+  const td2= document.createElement("td");
+  const author1 = document.createTextNode(book1[i].author);
+  td2.appendChild(author1);
+  const td3= document.createElement("td");
+  const page1= document.createTextNode(book1[i].pages);
+  td3.appendChild(page1);
+  const td4= document.createElement("td");
+  const bookread1= document.createTextNode(book1[i].bookread);
+  td4.appendChild(bookread1);
+  document.getElementById('books-list').appendChild(tr);
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
+  tr.appendChild(td4);
+  i++;
+
 
 }
